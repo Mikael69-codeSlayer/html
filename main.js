db0fcfcb1e3d4bebd005619926f7596003173f06
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-10-27 13:37:31 +0100

    My JS

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ class Person {
 
   sayHi() {
     return 'Hello my name is ' + this.name + ' and I am ' +
-      this.age + ' years old.';
+      this.age + ' years old. I\'m ' + (this.alive ? 'alive' :
+        'dead' + '!');
   }
 
 
@@ -21,3 +22,23 @@ let kalle = new Person('Kalle', 30);
 console.log(kalle);
 // Log the result of calling say hi
 console.log(kalle.sayHi());
+
+// Put three persons in an array
+let persons = [
+  new Person('Anna', 28),
+  new Person('Beata', 30),
+  new Person('Cecilia', 20)
+];
+
+// Add another person to the array
+// (arrays are dynamic - can change length in JS)
+persons.push(new Person('David', 5))
+
+// Change the property alive of Beata to false!
+persons[1].alive = false;
+
+
+// Loop through the array (for each is named for...of in JS)
+for (let person of persons) {
+  console.log(person.sayHi());
+}
